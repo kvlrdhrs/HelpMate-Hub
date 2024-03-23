f386ad0e5dc6205fab6e301996fb9c0cbe5e10c9
Kanan <kvlrdhrs@gmail.com>
2024-03-23 12:03:00 +0200

    TBLS and DB finished

--- Project/Data/create_TBLS.py
+++ Project/Data/create_TBLS.py
@@ -16,13 +16,12 @@ cursor.execute("""DROP TABLE if exists looking_for_volunteers""")
 create_table = """
     CREATE TABLE looking_for_volunteers(
     id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    phone VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
     city VARCHAR(100) NOT NULL,
-    gender VARCHAR(100),
-    age SMALLINT,
-    info TEXT NOT NULL
+    background_info TEXT,
+    tasks TEXT
     )
 """
 cursor.execute(create_table)
