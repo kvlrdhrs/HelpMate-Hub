c6caf3aaa5bc2442ec218a4df586e46127ece285
Kanan <kvlrdhrs@gmail.com>
2024-03-23 23:09:54 +0200

    1

--- Project/connect_/TG_connect.py
+++ Project/connect_/TG_connect.py
@@ -1,20 +1,37 @@
 from typing import Final
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, filters, ContextTypes, Application
+import asyncio
 import psycopg2
 
 TOKEN: Final = '7011755268:AAF7c9zDMwmAW1nN5ZDfkDktc9N8iweKFaM'
 BOT_USERNAME: Final = '@HelpMatch_bot'
 
 
+connection = psycopg2.connect(
+    host = '127.0.0.1',
+    port = '5432',
+    user = 'postgres',
+    password = '232323',
+    dbname = 'help_mm'
+)
+
+cursor=connection.cursor()
+connection.autocommit=True
+
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello! Thanks for creating me, Kanan!')
+  reply_markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+  reply_markup.add(['Volunteer', 'Volunteer Recruiter'])
+  await update.message.reply_text('Welcome to Help_MatchMaker! Choose your role:', reply_markup=reply_markup)
+
+async def main():
+  async with Application.builder().token(TOKEN).build() as app:
+    await app.initialize()  # Await initialization
+    try:
+      await app.run_polling()
+    finally:
+      await app.shutdown()  # Await shutdown
 
 
-
-if __name__== '__main__':
-    app = Application.builder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler('start', start_command))
-    app.run_polling(poll_interval=1)
-
+if __name__ == '__main__':
+  asyncio.run(main())
